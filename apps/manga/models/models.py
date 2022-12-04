8fa6168dcd183a1ca5754d5f6f191878d8b78271
Carlos Eduardo <suportebeloj@gmail.com>
2022-12-04 20:14:38 -0300

    initial models

--- apps/manga/models/models.py
+++ apps/manga/models/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Chapter(models.Model):
+    pass
+
+
+class Season(models.Model):
+    pass
+
+
+class Author(models.Model):
+    pass
+
+
+class Manga(models.Model):
+    title = models.CharField(max_length=255)
+    about = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_year = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
